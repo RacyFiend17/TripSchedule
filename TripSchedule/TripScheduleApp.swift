e2f9782f062ff2eea6eece92ab685c206ac38910
RacyFiend <demonperechemidi@gmail.com>
2026-02-26 00:22:28 +0300

    Initial Commit

---
+++ TripSchedule/TripScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripScheduleApp.swift
+//  TripSchedule
+//
+//  Created by Дмитрий Перчемиди on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TripScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripScheduleTests/TripScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripScheduleTests.swift
+//  TripScheduleTests
+//
+//  Created by Дмитрий Перчемиди on 26.02.2026.
+//
+
+import Testing
+@testable import TripSchedule
+
+struct TripScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
