76fc9e4042f0aa002e2924e5aea700c8d2d93630
RacyFiend <demonperechemidi@gmail.com>
2026-03-07 17:17:40 +0300

    Swipes

--- TripSchedule/StoriesView/StoriesView.swift
+++ TripSchedule/StoriesView/StoriesView.swift
@@ -2,10 +2,11 @@ import SwiftUI
 import Combine
 
 struct StoriesView: View {
+    
     struct Configuration {
         let timerTickInternal: TimeInterval
         let progressPerTick: CGFloat
-
+        
         init(
             storiesCount: Int,
             secondsPerStory: TimeInterval = 5,
@@ -23,18 +24,39 @@ struct StoriesView: View {
     @State private var progress: CGFloat = 0
     @State private var timer: Timer.TimerPublisher
     @State private var cancellable: Cancellable?
+    @State private var dragOffset: CGFloat = 0
     @Environment(\.dismiss) private var dismiss
     @Environment(StoriesManager.self) var storiesManager
-
+    
     init(storiesPack: StoryPack) {
         self.storiesPack = storiesPack
         configuration = Configuration(storiesCount: storiesPack.stories.count)
         timer = Self.createTimer(configuration: configuration)
     }
-
+    
     var body: some View {
         ZStack(alignment: .topTrailing) {
             StoryView(story: currentStory)
+                .offset(x: dragOffset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            dragOffset = value.translation.width
+                        }
+                        .onEnded { value in
+                            if value.translation.width < -50 {
+                                // Свайп влево - следующая
+                                nextStory()
+                            } else if value.translation.width > 50 {
+                                // Свайп вправо - предыдущая
+                                previousStory()
+                            }
+                            withAnimation {
+                                dragOffset = 0
+                            }
+                        }
+                )
+            
             ProgressBar(numberOfSections: storiesPack.stories.count, progress: progress)
                 .padding(.init(top: 28, leading: 12, bottom: 12, trailing: 12))
             CloseButton(action: {
@@ -43,9 +65,10 @@ struct StoriesView: View {
                 }
                 dismiss()
             })
-                .padding(.top, 57)
-                .padding(.trailing, 12)
+            .padding(.top, 57)
+            .padding(.trailing, 12)
         }
+        .background(Color(.storiesBackground))
         .onAppear {
             timer = Self.createTimer(configuration: configuration)
             cancellable = timer.connect()
@@ -61,15 +84,15 @@ struct StoriesView: View {
             resetTimer()
         }
     }
-
+    
     private func timerTick() {
         var nextProgress = progress + configuration.progressPerTick
         if nextProgress >= 1 {
             nextProgress = 0
         }
-            progress = nextProgress
+        progress = nextProgress
     }
-
+    
     private func nextStory() {
         let storiesCount = storiesPack.stories.count
         let currentStoryIndex = Int(progress * CGFloat(storiesCount))
@@ -78,18 +101,23 @@ struct StoriesView: View {
             progress = CGFloat(nextStoryIndex) / CGFloat(storiesCount)
         }
     }
-
+    
+    private func previousStory() {
+        let storiesCount = storiesPack.stories.count
+        let currentStoryIndex = Int(progress * CGFloat(storiesCount))
+        let previousStoryIndex = currentStoryIndex - 1 > 0 ? currentStoryIndex - 1 : 0
+        withAnimation {
+            progress = CGFloat(previousStoryIndex) / CGFloat(storiesCount)
+        }
+    }
+    
     private func resetTimer() {
         cancellable?.cancel()
         timer = Self.createTimer(configuration: configuration)
         cancellable = timer.connect()
     }
-
+    
     private static func createTimer(configuration: Configuration) -> Timer.TimerPublisher {
         Timer.publish(every: configuration.timerTickInternal, on: .main, in: .common)
     }
 }
-
-//#Preview {
-//    StoriesView()
-//}
