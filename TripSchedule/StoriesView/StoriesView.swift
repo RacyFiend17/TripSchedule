6f79bfbbc3c9bec62017bb9250c4c60566395f53
RacyFiend <demonperechemidi@gmail.com>
2026-03-07 17:23:34 +0300

    Made left and right taps for changing stories

--- TripSchedule/StoriesView/StoriesView.swift
+++ TripSchedule/StoriesView/StoriesView.swift
@@ -35,53 +35,70 @@ struct StoriesView: View {
     }
     
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            StoryView(story: currentStory)
-                .offset(x: dragOffset)
-                .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            dragOffset = value.translation.width
-                        }
-                        .onEnded { value in
-                            if value.translation.width < -50 {
-                                // Свайп влево - следующая
-                                nextStory()
-                            } else if value.translation.width > 50 {
-                                // Свайп вправо - предыдущая
-                                previousStory()
+        GeometryReader { geometry in
+            ZStack(alignment: .topTrailing) {
+                StoryView(story: currentStory)
+                    .offset(x: dragOffset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                dragOffset = value.translation.width
                             }
-                            withAnimation {
-                                dragOffset = 0
+                            .onEnded { value in
+                                if value.translation.width < -50 {
+                                    nextStory()
+                                } else if value.translation.width > 50 {
+                                    previousStory()
+                                }
+                                withAnimation {
+                                    dragOffset = 0
+                                }
                             }
+                    )
+                
+                HStack(spacing: 0) {
+                    Color.clear
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            previousStory()
+                            resetTimer()
                         }
-                )
-            
-            ProgressBar(numberOfSections: storiesPack.stories.count, progress: progress)
-                .padding(.init(top: 28, leading: 12, bottom: 12, trailing: 12))
-            CloseButton(action: {
-                if currentStoryIndex == storiesPack.stories.count - 1 {
-                    storiesManager.markAsWatched(packNumber: storiesPack.storyPackNumber)
+                        .frame(width: geometry.size.width * 0.2)
+                    
+                    Color.clear
+                        .frame(width: geometry.size.width * 0.6)
+
+                    Color.clear
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            nextStory()
+                            resetTimer()
+                        }
+                        .frame(width: geometry.size.width * 0.2)
                 }
-                dismiss()
-            })
-            .padding(.top, 57)
-            .padding(.trailing, 12)
-        }
-        .background(Color(.storiesBackground))
-        .onAppear {
-            timer = Self.createTimer(configuration: configuration)
-            cancellable = timer.connect()
-        }
-        .onDisappear {
-            cancellable?.cancel()
-        }
-        .onReceive(timer) { _ in
-            timerTick()
-        }
-        .onTapGesture {
-            nextStory()
-            resetTimer()
+                
+                ProgressBar(numberOfSections: storiesPack.stories.count, progress: progress)
+                    .padding(.init(top: 28, leading: 12, bottom: 12, trailing: 12))
+                CloseButton(action: {
+                    if currentStoryIndex == storiesPack.stories.count - 1 {
+                        storiesManager.markAsWatched(packNumber: storiesPack.storyPackNumber)
+                    }
+                    dismiss()
+                })
+                .padding(.top, 57)
+                .padding(.trailing, 12)
+            }
+            .background(Color(.storiesBackground))
+            .onAppear {
+                timer = Self.createTimer(configuration: configuration)
+                cancellable = timer.connect()
+            }
+            .onDisappear {
+                cancellable?.cancel()
+            }
+            .onReceive(timer) { _ in
+                timerTick()
+            }
         }
     }
     
@@ -96,7 +113,7 @@ struct StoriesView: View {
     private func nextStory() {
         let storiesCount = storiesPack.stories.count
         let currentStoryIndex = Int(progress * CGFloat(storiesCount))
-        let nextStoryIndex = currentStoryIndex + 1 < storiesCount ? currentStoryIndex + 1 : 0
+        let nextStoryIndex = currentStoryIndex + 1 < storiesCount ? currentStoryIndex + 1 : currentStoryIndex
         withAnimation {
             progress = CGFloat(nextStoryIndex) / CGFloat(storiesCount)
         }
@@ -120,4 +137,5 @@ struct StoriesView: View {
     private static func createTimer(configuration: Configuration) -> Timer.TimerPublisher {
         Timer.publish(every: configuration.timerTickInternal, on: .main, in: .common)
     }
+        
 }
